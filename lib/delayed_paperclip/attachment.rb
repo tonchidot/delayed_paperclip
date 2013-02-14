0cb8242eb315fb5d925430e29cae7dea42c4b71d
Tatsuya Takamura <takamura@tonchidot.com>
2013-02-14 19:44:04 +0900

    Don't update *_processing flag after processing method

--- lib/delayed_paperclip/attachment.rb
+++ lib/delayed_paperclip/attachment.rb
@@ -51,7 +51,6 @@ module DelayedPaperclip
         #instance.update_column("#{name}_processing", false) if instance.respond_to?(:"#{name}_processing?")
         if instance.respond_to?(:"#{name}_processing?")
           instance.send("#{name}_processing=", false)
-          instance.class.update_all({ "#{name}_processing" => false }, instance.class.primary_key => instance.id)
         end
       end
 
